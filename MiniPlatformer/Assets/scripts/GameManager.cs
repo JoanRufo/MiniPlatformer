82548ebc82612ad537af72caa6218fc04b52f74e
JoanRufo <jcarles2001@gmail.com>
2020-03-03 17:42:48 +0100

    vdgf

---
+++ MiniPlatformer/Assets/scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool playing = true;
+    public Text m_textPuntuacion;
+    public Text m_textVidas;
+    [HideInInspector] public int m_Puntuacion;
+    [HideInInspector] public int m_Vidas = 3;
+    public GameObject m_GameOverPanel;
+    public GameObject m_MenuPrincipal;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing == true)
+        {
+            m_textPuntuacion.text = "Puntos: " + m_Puntuacion;
+            m_textVidas.text = "Vidas: " + m_Vidas;
+
+            if (m_Vidas <= 0)
+            {
+                m_GameOverPanel.SetActive(true);
+
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        playing = true;
+
+        m_MenuPrincipal.SetActive(false);
+
+    }
+    public void EndLevel()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ MiniPlatformer/Assets/scripts/MovimientoPersonaje.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour {
+
+    private GameManager m_GameManager;
+    public float m_Speed;
+    // Use this for initialization
+    void Start () {
+
+        m_GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal")>0)
+        {
+            this.transform.position += this.transform.right * Time.deltaTime * m_Speed;
+
+
+        }
+
+        else if (Input.GetAxis("Horizontal")<0)
+        {
+
+
+            this.transform.position += -this.transform.right * Time.deltaTime * m_Speed;
+
+        }
+    }
+}
