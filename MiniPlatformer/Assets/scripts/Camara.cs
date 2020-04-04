2fc9c2691956bf5870c39926395c4e9f8832e450
ArnauGarcis <arnaugallecs@gmail.com>
2020-04-04 23:35:17 +0200

    update

---
+++ MiniPlatformer/Assets/scripts/Camara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour {
+
+    public GameObject personaje;
+    private Vector3 posicion;
+
+
+	
+	void Start () {
+
+        posicion = transform.position - personaje.transform.position;
+		
+	}
+	
+	
+	void Update () {
+
+        transform.position = personaje.transform.position + posicion;
+		
+	}
+}
